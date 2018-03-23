cf9d1651663fe416af3b29eea397189b1a9d7180
osboxes.org <l7shao@uwaterloo.ca>
2018-03-23 00:15:56 -0400

    tree model

--- demo1.py
+++ demo1.py
@@ -2,12 +2,12 @@
 
 import random
 import numpy
+from sklearn import tree
+from random import randint
+import graphviz
 
 #generating dataset 
-from random import randint
 mydir=[]
-
-
 my_train_points=[]
 
 for i in range(100):
@@ -23,40 +23,24 @@ for i in range(100):
 	
 #generate prediction dataset
 preds = []
-for i in range(10):
+for i in range(100):
 	subdir=[]
-	for i in range(10):
+	for i in range(100):
 		subdir.append(randint(0,9))
 	preds.append(subdir)
 
-results=clf.predict(preds)
-print results
-#clf.predict(preds)##ｉｆ　ｗｅ　ｗａｎｔ　ｔｏ　ｆｉｎｄ　ｏｕｔ　ｔｈｅ　ｐｒｏｂａｂｉｌｉｔｙ
 
-
-
-
-
-
-
-'''
-print len(my_train_points)
-print labels
-'''
-
-#build the decision tree model:
-from sklearn import tree
+#train the decision tree model:
 X = my_train_points
 Y = labels
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(X, Y)
 
-from sklearn.datasets import load_iris
-from sklearn import tree
+#print predicate results
+results=clf.predict(preds)
+print results
 
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X, Y)
-import graphviz 
+#generate reports
 dot_data = tree.export_graphviz(clf, out_file=None) 
 graph = graphviz.Source(dot_data) 
 graph.render("decision tree") 
